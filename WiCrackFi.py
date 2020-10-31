f1f370e9cc9ed789eae623d553da8702177181bb
Pedro Gomes <23638870+ShineZex@users.noreply.github.com>
2020-10-31 03:49:14 +0000

    Update WiCrackFi.py
    Fixed Error from line 4 "Non-ASCII character '\xc2'"

--- WiCrackFi.py
+++ WiCrackFi.py
@@ -1,7 +1,7 @@
 # ====================================================================================================================
 #                                                   WiCrackFi
 #                               WiFi Security Testing Tool Created By Pedro Gomes - 2020
-#                                     © Copyright 2020 - All Rights Reserved
+#                                       Copyright 2020 - All Rights Reserved
 # ====================================================================================================================
 # WiFi Security Testing Tool that automates the use of Aircrack-ng suit and makes testing WiFi Security much easier!
 # ====================================================================================================================
